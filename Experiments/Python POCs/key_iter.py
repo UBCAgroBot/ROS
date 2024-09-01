821d8420ce0469a35f4468329e93790665dc50aa
Ishaan Datta <ishaandatta737@gmail.com>
2024-09-01 12:14:41 -0700

    view all module data

--- Experiments/Python POCs/key_iter.py
+++ Experiments/Python POCs/key_iter.py
@@ -13,3 +13,41 @@ with jtop() as jetson:
         print("------ TOTAL ------")
         for key, value in total.items():
             print("{key}: {value}".format(key=key, value=value))
+            
+        print("overview!!!")
+        # CPU
+        print('*** CPUs ***')
+        print(jetson.cpu)
+        # CPU
+        print('*** Memory ***')
+        print(jetson.memory)
+        # GPU
+        print('*** GPU ***')
+        print(jetson.gpu)
+        # Engines
+        print('*** engine ***')
+        print(jetson.engine)
+        # nvpmodel
+        print('*** NV Power Model ***')
+        print(jetson.nvpmodel)
+        # jetson_clocks
+        print('*** jetson_clocks ***')
+        print(jetson.jetson_clocks)
+        # Status disk
+        print('*** disk ***')
+        print(jetson.disk)
+        # Status fans
+        print('*** fan ***')
+        print(jetson.fan)
+        # uptime
+        print('*** uptime ***')
+        print(jetson.uptime)
+        # local interfaces
+        print('*** local interfaces ***')
+        print(jetson.local_interfaces)
+        # Temperature
+        print('*** temperature ***')
+        print(jetson.temperature)
+        # Power
+        print('*** power ***')
+        print(jetson.power)
