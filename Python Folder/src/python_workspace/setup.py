5b0907417839bf1a81660e24fe17e2ee57ffe2cd
Ishaan Datta <ishaandatta737@gmail.com>
2024-09-10 18:20:09 -0700

    updated setup.py

--- Python Folder/src/python_workspace/setup.py
+++ Python Folder/src/python_workspace/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'python_workspace'
@@ -9,15 +11,12 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        # Include our package.xml file
+        (os.path.join('share', package_name), ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
     ],
-    install_requires=[
-        'setuptools',
-        'rclpy', 
-        'numpy', 
-        'pycuda', 
-        'tensorrt'
-    ],
+    install_requires=['setuptools',],
     zip_safe=True,
     maintainer='ishaan_datta',
     maintainer_email='ishaandatta737@gmail.com',
