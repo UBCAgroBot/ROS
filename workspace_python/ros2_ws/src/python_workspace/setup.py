d83f37b4ce6d767df512ce4b3c7c590866f7d015
Ishaan Datta <Ishaandatta737@gmail.com>
2024-10-29 19:38:31 -0700

    dev container changes + poc code

--- workspace_python/ros2_ws/src/python_workspace/setup.py
+++ workspace_python/ros2_ws/src/python_workspace/setup.py
@@ -3,12 +3,11 @@ from glob import glob
 from setuptools import setup
 
 package_name = 'python_workspace'
-subfolder = f'{package_name}/scripts'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name,subfolder],
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -25,7 +24,6 @@ setup(
             'video_node = python_workspace.video_node:main',
             'camera_node = python_workspace.zed_camera_node:main',
             'picture_node = python_workspace.picture_node:main',
-            'inference_node = python_workspace.inference_node:main',
             'jetson_node = python_workspace.jetson_node:main',
             'extermination_node = python_workspace.extermination_node:main'
         ],
