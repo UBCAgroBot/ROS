8c2dcfb7d93691fc4ce3d5667c6bf298a32ba672
Ishaan Datta <Ishaandatta737@gmail.com>
2024-11-08 18:45:56 -0800

    test serial test

--- tools/utils/serial_test.py
+++ tools/utils/serial_test.py
@@ -1,16 +1,28 @@
 import serial
 import time
 import serial.tools.list_ports
+import argparse
 
-ports = serial.tools.list_ports.comports()
-for port in ports:
-    print(port.device)
+def display_ports():
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        print(port.device)
 
 # /dev/ttyUSB0 for linux?
-ser = serial.Serial('COM3', 115200, timeout=1)  # Adjust USB port as needed
 
-while True:
-    serialized_msg = str(1) + '\n'  # Add a newline as a delimiter
-    ser.write(serialized_msg.encode())
-    print("sent to arduino")
-    time.sleep(1)
+def connect(port):
+    ser = serial.Serial('COM3', 115200, timeout=1)  # Adjust USB port as needed
+    while True:
+        serialized_msg = str(1) + '\n'  # Add a newline as a delimiter
+        ser.write(serialized_msg.encode())
+        print("sent to arduino")
+        time.sleep(1)
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Serial port communication')
+    parser.add_argument('--port', type=str, required=True, help='Serial port to connect to')
+    args = parser.parse_args()
+    
+    display_ports()
+    
+    connect(args.port)
