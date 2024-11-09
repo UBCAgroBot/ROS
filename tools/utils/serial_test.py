a1181470ee24d7d3269df7d447a6dd2b0784ed50
Ishaan Datta <Ishaandatta737@gmail.com>
2024-11-08 18:37:24 -0800

    serial test change successful

--- tools/utils/serial_test.py
+++ tools/utils/serial_test.py
@@ -6,11 +6,11 @@ ports = serial.tools.list_ports.comports()
 for port in ports:
     print(port.device)
 
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)  # Adjust USB port as needed
+# /dev/ttyUSB0 for linux?
+ser = serial.Serial('COM3', 115200, timeout=1)  # Adjust USB port as needed
 
 while True:
     serialized_msg = str(1) + '\n'  # Add a newline as a delimiter
     ser.write(serialized_msg.encode())
     print("sent to arduino")
-    
-    time.sleep(2)
+    time.sleep(1)
