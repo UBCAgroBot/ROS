2b000950d5459888e3da322c06c3cb2656f70768
vangeliq <valeryangelique2001@gmail.com>
2025-02-03 20:21:51 -0800

    added changes to util

--- ros2_ws/src/python_package/python_package/picture_node.py
+++ ros2_ws/src/python_package/python_package/picture_node.py
@@ -6,12 +6,13 @@ from rclpy.time import Time
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 from sensor_msgs.msg import Image
-from std_msgs.msg import Header, String, Int8
+from std_msgs.msg import Header, String
 from cv_bridge import CvBridge
 import queue
 import numpy as np
 
-from .scripts.utils import ModelInference
+from .scripts.utils import preprocess
+
 
 from custom_interfaces.msg import ImageInput                            # CHANGE
 # node to publish static image data to the /image topic. 
@@ -20,10 +21,11 @@ class PictureNode(Node):
     def __init__(self):
         super().__init__('picture_node')
 
-        self.declare_parameter('static_image_path', '/home/user/ROS/assets/maize/IMG_1822_14.JPG')
-        self.declare_parameter('loop', -1)  # 0 = don't loop, >0 = # of loops, -1 = loop forever
+        self.declare_parameter('static_image_path', '/home/usr/Desktop/ROS/assets/IMG_1822_14.JPG')
+        self.declare_parameter('loop', 0)  # 0 = don't loop, >0 = # of loops, -1 = loop forever
         self.declare_parameter('frame_rate', 1)  # Desired frame rate for publishing
 
+        
         self.static_image_path = self.get_parameter('static_image_path').get_parameter_value().string_value
         # Resolve the path programmatically
         if not os.path.isabs(self.static_image_path):
@@ -33,17 +35,17 @@ class PictureNode(Node):
         self.frame_rate = self.get_parameter('frame_rate').get_parameter_value().integer_value
 
         self.bridge = CvBridge()
-        self.model = ModelInference()
 
         self.image_list = self.get_images()
         self.loop_count = 0
         self.image_counter = 0
 
+
         self.input_image_publisher = self.create_publisher(ImageInput, 'input_image', 10)
         timer_period = 1/self.frame_rate  # publish every 0.5 seconds
         self.timer = self.create_timer(timer_period * 2, self.publish_static_image)
 
-
+           
     def get_images(self)-> list[np.ndarray]:
         """
         Returns a list of images in the form of cv images 
@@ -54,9 +56,7 @@ class PictureNode(Node):
         if not os.path.exists(self.static_image_path):
             self.get_logger().error(f"Static image not found at {self.static_image_path}")
             raise FileNotFoundError(f"Static image not found at {self.static_image_path}")
-
-        filename = self.static_image_path
-
+        
         image_paths = []
         if os.path.isfile(self.static_image_path) \
             and (filename.endswith('.JPG') or filename.endswith('.png')):
@@ -71,7 +71,7 @@ class PictureNode(Node):
         if len(image_paths) == 0:
             self.get_logger().error(f"No images found at {self.static_image_path}")
             return
-
+        
         images = []
         for filename in image_paths:
             # read images
@@ -79,13 +79,13 @@ class PictureNode(Node):
             if image is None:
                 self.get_logger().error(f"Failed to read image: {filename}")
                 raise FileNotFoundError(f"Failed to read image: {filename}")
-
+                        
             # add to the list of images
             images.append(image)
 
         return images
 
-
+    
     def publish_static_image(self):
         """
         Publishes static images to the /image topic
@@ -101,7 +101,7 @@ class PictureNode(Node):
             raw_image = self.image_list[position]
 
             #todo:  create the message to publish
-            postprocessed_img = self.model.preprocess(raw_image)
+            postprocessed_img = preprocess(raw_image)
             postprocessed_img_msg = self.bridge.cv2_to_imgmsg(postprocessed_img, encoding='rgb8')
             raw_img_msg = self.bridge.cv2_to_imgmsg(raw_image, encoding='rgb8')
 
@@ -114,10 +114,9 @@ class PictureNode(Node):
 
             # publish image and increment whatever is needed
             self.input_image_publisher.publish(image_input)
-            self.get_logger().info(f"Published image {self.image_counter}")
+
             self.image_counter += 1
             self.loop_count = self.image_counter // array_size
-
         else:
             # stop the timer/quit the node
             self.timer.cancel()
@@ -136,12 +135,9 @@ def main(args=None):
     picture_node = PictureNode()
     executor = MultiThreadedExecutor(num_threads=1)
     executor.add_node(picture_node)
-
+    
     try:
         executor.spin()
-    except KeyboardInterrupt:
-        print("Shutting down picture node")
-        return
     finally:
         executor.shutdown()
         picture_node.destroy_node()
